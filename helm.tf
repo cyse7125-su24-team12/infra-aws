41b2d3a4e8e93fb7afeb243a1135e35b8e58e232
Shyam Sundar Mylai <mylai.s@northeastern.edu>
2024-06-25 07:10:57 -0400

    feat: adding kubernetes& helm to eks bootstrap

--- helm.tf
+++ helm.tf
@@ -4,12 +4,70 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_data.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster_auth.token
   }
+  alias = "helm-eks"
 }
 
-# resource "helm_release" "test_release" {
-#     name       = "test-release"
-#     repository = "https://charts.bitnami.com/bitnami"
-#     chart      = "nginx"
-#     depends_on = [kubernetes_namespace.namespace1, kubernetes_namespace.namespace2, kubernetes_namespace.namespace3]
-#     namespace  = "default"
-# }
+resource "helm_release" "postgresql-ha-release" {
+  name       = "postgresql-ha-release"
+  repository = "https://charts.bitnami.com/bitnami"
+  provider   = helm.helm-eks
+  chart      = "postgresql-ha"
+  depends_on = [kubernetes_namespace.namespace1, kubernetes_namespace.namespace2, kubernetes_namespace.namespace3]
+  namespace  = "namespace3"
+  set {
+    name  = "fullnameOverride"
+    value = "cve-webapp-processor"
+  }
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "persistance.storageClass"
+    value = "ebc-sc"
+  }
+
+  set{
+    name = "persistence.size"
+    value = "4Gi"
+  }
+
+  set{
+    name = "postgresql.database"
+    value = "cve"
+  }
+
+  set{
+    name = "postgresql.username"
+    value = "cve_user"
+  }
+
+  set{
+    name = "postgresql.password"
+    value = "cve123"
+  }
+
+  set{
+    name = "postgresql.repmgrUsername"
+    value = "repmgr"
+  }
+
+  set{
+    name = "postgresql.repmgrPassword"
+    value = "repmgr123"
+  }
+
+  set{
+    name = "pgpool.adminPassword"
+    value = "admin123"
+  }
+
+
+  set{
+    name = "pgpool.adminUsername"
+    value = "pgadmin"
+  }
+
+}
--- kubernetes.tf
+++ kubernetes.tf
@@ -1,35 +1,61 @@
 data "aws_eks_cluster" "cluster_data" {
-    depends_on = [ module.eks ]
-
-  name = module.eks.cluster_name
+  depends_on = [module.eks]
+  name       = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster_auth" {
-  name = module.eks.cluster_name
+  depends_on = [module.eks]
+  name       = module.eks.cluster_name
 }
 
 
 provider "kubernetes" {
-    host                   = data.aws_eks_cluster.cluster_data.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_data.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster_auth.token
-    # version                = "~> 1.11"  
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_data.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+  alias                  = "kubernetes-eks"
+  # version                = "~> 1.11"  
 }
 
 resource "kubernetes_namespace" "namespace1" {
+  depends_on = [module.eks, module.ebs_csi_irsa_role]
+  provider   = kubernetes.kubernetes-eks
   metadata {
     name = "namepsace1"
   }
 }
 
 resource "kubernetes_namespace" "namespace2" {
-    metadata {
-        name = "namepsace2"
-    }
+  depends_on = [module.eks, module.ebs_csi_irsa_role]
+  provider   = kubernetes.kubernetes-eks
+
+  metadata {
+    name = "namepsace2"
+  }
 }
 
 resource "kubernetes_namespace" "namespace3" {
-    metadata {
-        name = "namepsace3"
-    }
+  depends_on = [module.eks, module.ebs_csi_irsa_role]
+  provider   = kubernetes.kubernetes-eks
+
+  metadata {
+    name = "namespace3"
+  }
+}
+
+
+resource "kubernetes_storage_class" "pvc_sc" {
+  depends_on = [aws_kms_key.key_for_ebs_volume, module.eks]
+  provider   = kubernetes.kubernetes-eks
+  metadata {
+    name = "ebs-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  reclaim_policy      = "Retain"
+  volume_binding_mode = "Immediate"
+  parameters = {
+    type      = "gp2"
+    encrypted = "true"
+    kmsKeyId  = aws_kms_key.key_for_ebs_volume.arn
+  }
 }
