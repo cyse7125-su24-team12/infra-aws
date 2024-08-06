5b0cddf6418f2604c7bc250800d644ca401333d3
Shyam Sundar Mylai <mylai.s@northeastern.edu>
2024-08-06 13:09:33 -0400

    chore: minor updated

--- kubernetes.tf
+++ kubernetes.tf
@@ -304,43 +304,43 @@ resource "kubernetes_namespace" "cert_manager" {
   }
 }
 
-resource "kubernetes_manifest" "istio_ingress_gateway" {
-  depends_on = [module.eks]
-  provider   = kubernetes.kubernetes-eks
-  # namespace  = kubernetes_namespace.prometheus_graphana_ns.metadata[0].name
-  manifest = yamldecode(file("${path.module}/manifests/ingress-gateway.yaml"))
-}
+# resource "kubernetes_manifest" "istio_ingress_gateway" {
+#   depends_on = [module.eks]
+#   provider   = kubernetes.kubernetes-eks
+#   # namespace  = kubernetes_namespace.prometheus_graphana_ns.metadata[0].name
+#   manifest = yamldecode(file("${path.module}/manifests/ingress-gateway.yaml"))
+# }
 
-resource "kubernetes_manifest" "virtual_service_grafana" {
-  depends_on = [module.eks]
-  provider   = kubernetes.kubernetes-eks
-  manifest   = yamldecode(file("${path.module}/manifests/virtual-service-grafana.yaml"))
-}
+# resource "kubernetes_manifest" "virtual_service_grafana" {
+#   depends_on = [module.eks]
+#   provider   = kubernetes.kubernetes-eks
+#   manifest   = yamldecode(file("${path.module}/manifests/virtual-service-grafana.yaml"))
+# }
 
-resource "kubernetes_manifest" "certificate_issuer" {
-  depends_on = [module.eks, helm_release.cert_manager]
-  provider   = kubernetes.kubernetes-eks
-  manifest   = yamldecode(file("${path.module}/manifests/certificate-issuer.yaml"))
-}
+# resource "kubernetes_manifest" "certificate_issuer" {
+#   depends_on = [module.eks, helm_release.cert_manager]
+#   provider   = kubernetes.kubernetes-eks
+#   manifest   = yamldecode(file("${path.module}/manifests/certificate-issuer.yaml"))
+# }
 
 
-resource "kubernetes_manifest" "certificate_values" {
-  depends_on = [module.eks, helm_release.cert_manager, kubernetes_manifest.certificate_issuer]
-  provider   = kubernetes.kubernetes-eks
-  manifest   = yamldecode(file("${path.module}/manifests/certificate-values.yaml"))
-}
+# resource "kubernetes_manifest" "certificate_values" {
+#   depends_on = [module.eks, helm_release.cert_manager, kubernetes_manifest.certificate_issuer]
+#   provider   = kubernetes.kubernetes-eks
+#   manifest   = yamldecode(file("${path.module}/manifests/certificate-values.yaml"))
+# }
 
-resource "kubernetes_manifest" "cert_mgr_role" {
-  depends_on = [module.eks, helm_release.cert_manager]
-  provider   = kubernetes.kubernetes-eks
-  manifest   = yamldecode(file("${path.module}/manifests/cert-mgr-role.yaml"))
-}
+# resource "kubernetes_manifest" "cert_mgr_role" {
+#   depends_on = [module.eks, helm_release.cert_manager]
+#   provider   = kubernetes.kubernetes-eks
+#   manifest   = yamldecode(file("${path.module}/manifests/cert-mgr-role.yaml"))
+# }
 
-resource "kubernetes_manifest" "cert_mgr_rolebinding" {
-  depends_on = [module.eks, helm_release.cert_manager, kubernetes_manifest.cert_mgr_role]
-  provider   = kubernetes.kubernetes-eks
-  manifest   = yamldecode(file("${path.module}/manifests/cert-mgr-rolebinding.yaml"))
-}
+# resource "kubernetes_manifest" "cert_mgr_rolebinding" {
+#   depends_on = [module.eks, helm_release.cert_manager, kubernetes_manifest.cert_mgr_role]
+#   provider   = kubernetes.kubernetes-eks
+#   manifest   = yamldecode(file("${path.module}/manifests/cert-mgr-rolebinding.yaml"))
+# }
 
 data "kubernetes_service" "istio_gateway_service_data" {
   depends_on = [helm_release.istio_gateway, module.eks, ]
